bed0314964b4a98947e688a4febe8107c75e87ec
Katter <narcat99@gmail.com>
2024-09-21 10:10:00 +0500

    Add a ConsoleSample (I'm commiting this much later than actually writing the code, and I saw some weird unreasonable code that causes problems in the sample, will address these issues in next commit)

---
+++ Katter.HotKeys.ConsoleSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using Katter.HotKeys;
+using Katter.HotKeys.Behaviours;
+using Katter.HotKeys.SharpHook;
+using SharpHook;
+using SharpHook.Native;
+using SharpHook.Reactive;
+
+SimpleReactiveGlobalHook hook = new(GlobalHookType.Keyboard);
+SharpHookKeyboardKeyManager keyManager = new(hook);
+SharpHookKeyModifiersManager modifiersManager = new(keyManager);
+SharpHookKeyGestureManager keyGestureManager = new(modifiersManager, keyManager);
+HotKeyBindingsManager<KeyGesture> hotKeyBindingsManager = new(keyGestureManager);
+KeyGesture gesture = new(KeyCode.VcQ, KeyModifiers.Control | KeyModifiers.Shift);
+ActionTapBindingBehaviour tapBehaviour = new(() => Console.WriteLine($"Tapped {gesture}!"));
+ActionHoldBindingBehaviour holdBehaviour = new(token =>
+{
+	var start = DateTime.UtcNow;
+	while (!token.IsCancellationRequested)
+	{
+		Console.Clear();
+		Console.WriteLine($"Hold {gesture} for {DateTime.UtcNow - start}!");
+		Thread.Sleep(1);
+	}
+	Console.Clear();
+	Console.WriteLine($"Hold {gesture} for {DateTime.UtcNow - start}!");
+});
+hotKeyBindingsManager.CreateBinding("Tap", tapBehaviour, gesture);
+hotKeyBindingsManager.CreateBinding("Hold", holdBehaviour, gesture);
+hook.Run();
