25a5cd0c06f6b8aed481c7a233814ae8a7ab49c9
Katter <narcat99@gmail.com>
2024-09-23 13:51:00 +0500

    temporal crutch

--- HotKeys.SharpHook/SharpHookMouseButtonsManager.cs
+++ HotKeys.SharpHook/SharpHookMouseButtonsManager.cs
@@ -13,7 +13,16 @@ public sealed class SharpHookMouseButtonsManager : KeyManager<MouseButton>
 	public SharpHookMouseButtonsManager(IReactiveGlobalHook hook)
 	{
 		KeyPressed = hook.MousePressed.Select(TransformArgs);
-		KeyReleased = hook.MouseReleased.Select(TransformArgs);
+		KeyReleased = hook.MouseReleased.Select(TransformArgs)
+			// https://github.com/TolikPylypchuk/SharpHook/issues/119
+			.Select(button =>
+			{
+				if (button == MouseButton.Button2)
+					return MouseButton.Button3;
+				if (button == MouseButton.Button3)
+					return MouseButton.Button2;
+				return button;
+			});
 	}
 
 	private static MouseButton TransformArgs(MouseHookEventArgs args)
