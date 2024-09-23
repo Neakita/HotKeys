961b2f0f23eea87a2f7c5f2dc32808da0b8c2961
Katter <narcat99@gmail.com>
2024-09-23 17:31:52 +0500

    FormattedSharpHookMouseButton

---
+++ HotKeys.SharpHook/FormattedSharpHookMouseButton.cs
@@ -0,0 +1,10 @@
+namespace HotKeys.SharpHook;
+
+public enum FormattedSharpHookMouseButton : ushort
+{
+	LButton = 1,
+	RButton = 2,
+	MButton = 3,
+	XButton1 = 4,
+	XButton2 = 5
+}
--- HotKeys.SharpHook/SharpHookMouseButtonsManager.cs
+++ HotKeys.SharpHook/SharpHookMouseButtonsManager.cs
@@ -5,28 +5,38 @@ using SharpHook.Reactive;
 
 namespace HotKeys.SharpHook;
 
-public sealed class SharpHookMouseButtonsManager : KeyManager<MouseButton>
+public sealed class SharpHookMouseButtonsManager : KeyManager<FormattedSharpHookMouseButton>
 {
-	public IObservable<MouseButton> KeyPressed { get; }
-	public IObservable<MouseButton> KeyReleased { get; }
+	public IObservable<FormattedSharpHookMouseButton> KeyPressed { get; }
+	public IObservable<FormattedSharpHookMouseButton> KeyReleased { get; }
 
 	public SharpHookMouseButtonsManager(IReactiveGlobalHook hook)
 	{
-		KeyPressed = hook.MousePressed.Select(TransformArgs);
-		KeyReleased = hook.MouseReleased.Select(TransformArgs)
-			// https://github.com/TolikPylypchuk/SharpHook/issues/119
-			.Select(button =>
-			{
-				if (button == MouseButton.Button2)
-					return MouseButton.Button3;
-				if (button == MouseButton.Button3)
-					return MouseButton.Button2;
-				return button;
-			});
+		KeyPressed = hook.MousePressed
+			.Select(TransformArgs)
+			.Select(TransformToFormatted);
+		KeyReleased = hook.MouseReleased
+			.Select(TransformArgs)
+			.Select(SwapButton2AndButton3) // https://github.com/TolikPylypchuk/SharpHook/issues/119
+			.Select(TransformToFormatted);
 	}
 
 	private static MouseButton TransformArgs(MouseHookEventArgs args)
 	{
 		return args.Data.Button;
+	}
+
+	private static MouseButton SwapButton2AndButton3(MouseButton button)
+	{
+		if (button == MouseButton.Button2)
+			return MouseButton.Button3;
+		if (button == MouseButton.Button3)
+			return MouseButton.Button2;
+		return button;
+	}
+
+	private static FormattedSharpHookMouseButton TransformToFormatted(MouseButton button)
+	{
+		return (FormattedSharpHookMouseButton)button;
 	}
 }
