52ff54d8702eb3f3ec648d5d7db33f62074c6eae
Katter <narcat99@gmail.com>
2024-09-23 16:00:08 +0500

    AggregateKeyManagerTests

---
+++ HotKeys.Tests/AggregateKeyManagerTests.cs
@@ -0,0 +1,28 @@
+using CommunityToolkit.Diagnostics;
+using FluentAssertions;
+
+namespace HotKeys.Tests;
+
+public sealed class AggregateKeyManagerTests
+{
+	[Fact]
+	public void ShouldMergeAllEvents()
+	{
+		using ControlledKeyManager firstKeyManager = new();
+		using ControlledKeyManager secondKeyManager = new();
+		AggregateKeyManager aggregateKeyManager = new([firstKeyManager, secondKeyManager]);
+		HashSet<object> pressedKeys = new();
+		aggregateKeyManager.KeyPressed.Subscribe(key => Guard.IsTrue(pressedKeys.Add(key)));
+		aggregateKeyManager.KeyReleased.Subscribe(key => Guard.IsTrue(pressedKeys.Remove(key)));
+		firstKeyManager.NotifyPressed('D');
+		pressedKeys.Should().Contain('D');
+		secondKeyManager.NotifyPressed('F');
+		pressedKeys.Should().Contain(['D', 'F']);
+		Assert.ThrowsAny<Exception>(() => firstKeyManager.NotifyPressed('D'));
+		secondKeyManager.NotifyReleased('F');
+		pressedKeys.Should().Contain('D');
+		firstKeyManager.NotifyReleased('D');
+		pressedKeys.Should().BeEmpty();
+		Assert.ThrowsAny<Exception>(() => secondKeyManager.NotifyReleased('F'));
+	}
+}
