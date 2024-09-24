0fd9d82e539100b1f14433fb1b5d00e247cd0047
Katter <narcat99@gmail.com>
2024-09-24 09:12:02 +0500

    Rework InputType to InputTypes flags enum

--- HotKeys/InputTypes.cs
+++ HotKeys/InputTypes.cs
@@ -1,33 +1,43 @@
 ﻿namespace HotKeys;
 
-public enum InputType
+public enum InputTypes : byte
 {
 	/// <summary>
 	/// Triggers only when a button is pushed down and released quickly.
 	/// </summary>
-	Tap,
+	Tap = 1 << 0,
+
 	/// <summary>
 	/// Triggers on the second press if you quickly push down the same button twice.
 	/// </summary>
-	DoubleTap,
+	DoubleTap = 1 << 1,
+
 	/// <summary>
 	/// Triggers as soon as a button is pushed down.
 	/// </summary>
-	Press,
+	Press = 1 << 2,
+
 	/// <summary>
 	/// Triggers after a button has been pushed down for a set amount of time.
 	/// </summary>
-	LongPress,
+	LongPress = 1 << 3,
+
 	/// <summary>
 	/// Triggers as soon as a button is released.
 	/// </summary>
-	Release,
+	Release = 1 << 4,
+
 	/// <summary>
 	/// Triggers continuously while a button is being pushed down.
 	/// </summary>
-	Hold,
+	Hold = 1 << 5,
+
 	/// <summary>
 	/// After a button has been pushed down for a set amount of time, this starts triggering continuously.
 	/// </summary>
-	LongHold
+	LongHold = 1 << 6,
+
+	AllOneTime = Tap | DoubleTap | Press | LongPress | Release,
+	AllContinuous = Hold | LongHold,
+	All = AllOneTime | AllContinuous
 }
