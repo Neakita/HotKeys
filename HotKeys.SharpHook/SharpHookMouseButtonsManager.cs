c6d36c0881d70ef997b63706f4ad140c5bb571dd
Katter <narcat99@gmail.com>
2024-10-17 17:12:49 +0500

    Bump packages

--- HotKeys.SharpHook/SharpHookMouseButtonsManager.cs
+++ HotKeys.SharpHook/SharpHookMouseButtonsManager.cs
@@ -17,22 +17,12 @@ public sealed class SharpHookMouseButtonsManager : KeyManager<FormattedSharpButt
 			.Select(TransformToFormatted);
 		KeyReleased = hook.MouseReleased
 			.Select(TransformArgs)
-			.Select(SwapButton2AndButton3) // https://github.com/TolikPylypchuk/SharpHook/issues/119
 			.Select(TransformToFormatted);
 	}
 
 	private static MouseButton TransformArgs(MouseHookEventArgs args)
 	{
 		return args.Data.Button;
-	}
-
-	private static MouseButton SwapButton2AndButton3(MouseButton button)
-	{
-		if (button == MouseButton.Button2)
-			return MouseButton.Button3;
-		if (button == MouseButton.Button3)
-			return MouseButton.Button2;
-		return button;
 	}
 
 	private static FormattedSharpButton TransformToFormatted(MouseButton button)
