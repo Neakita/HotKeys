0b9c174607b85ca0b6c5a08fbf8677fa4dd4d87e
Neakita <thekatterdev@gmail.com>
2025-02-14 17:57:34 +0500

    Add params to AggregateKeyManager constructor parameter

--- HotKeys/AggregateKeyManager.cs
+++ HotKeys/AggregateKeyManager.cs
@@ -7,7 +7,7 @@ public sealed class AggregateKeyManager : KeyManager
 	public IObservable<object> KeyPressed { get; }
 	public IObservable<object> KeyReleased { get; }
 
-	public AggregateKeyManager(IReadOnlyCollection<KeyManager> keyManagers)
+	public AggregateKeyManager(params IReadOnlyCollection<KeyManager> keyManagers)
 	{
 		KeyPressed = keyManagers.Select(keyManager => keyManager.KeyPressed).Merge();
 		KeyReleased = keyManagers.Select(keyManager => keyManager.KeyReleased).Merge();
